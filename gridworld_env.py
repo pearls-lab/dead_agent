0f5e9c65ad26484d34666b2ce9d7d9969e71d3f3
Chris C <ccui46@gatech.edu>
2025-06-02 17:53:15 +0000

    Integrated chord arrays

--- gridworld_env.py
+++ gridworld_env.py
@@ -8,6 +8,7 @@ import utils
 from distutils.util import strtobool
 import pickle
 import random
+from target_chords import TARGET_CHORD
 
 
 class GridWorldEnv(gym.Env):
@@ -18,18 +19,20 @@ class GridWorldEnv(gym.Env):
         self.observation_space    = gym.spaces.Box(low = --1, high = 100, shape=(600,), dtype = float)
         # Each # represents a specific chord (this mapping is automatically done when converting to a midi file.)
         self.CLASS_LIST = [43, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81]
+        # Hard coded for speed:
+        self.TARGET_CHORD = TARGET_CHORD
+
         self.idx_to_chord = {}
         # Get idx to chord mapping:
-        for i in range(len(self.CLASS_LIST)):
+        for i in range(2, len(self.CLASS_LIST)):
             self.idx_to_chord[i] = self.CLASS_LIST[i]
-        self.action_space         = gym.spaces.Discrete(len(self.CLASS_LIST)) # For each of the most common chords + <start> and <end>
+        self.action_space         = gym.spaces.Discrete(len(self.CLASS_LIST) + 2) # For each of the most common chords + <end> + 'no chord'
 
         # Internal metrics
         self.steps                = 0
         self.cumulative_reward    = 0
         self.max_steps            = 600
-        self.current_obs          = [-1] * self.max_steps
-        self.target_chord         = []
+        self.current_obs          = np.array([-1] * self.max_steps)
         self.all_locs              = []
         self.game_locs             = []
         self.all_traj              = []
@@ -37,20 +40,16 @@ class GridWorldEnv(gym.Env):
         self.current_acts          = []
         self.subob_traj            = []
         self.total_resets          = 0
+
     
     def _get_obs(self):
-        return self.target_chord[:self.steps] + [-1] * (self.max_steps - self.steps)
+        return np.array(self.TARGET_CHORD[:self.steps] + [-1] * (self.max_steps - self.steps))
         
     
     def reset(self, seed: Optional[int] = None, options: Optional[dict] = None):
         # We need the following line to seed self.np_random
         super().reset(seed=seed)
-        # print(self.masterRewardDict)
         # Reset Internal Metrics
-        # Because I am very tired, just try randomly generating this to see if it works:
-        for i in range(self.max_steps):
-            self.target_chord.append(random.randint(1, 93))
-        print(self.target_chord)
 
         self.current_acts      = []
         self.current_traj      = []
@@ -58,8 +57,7 @@ class GridWorldEnv(gym.Env):
         self.cumulative_reward = 0
 
         self.current_obs = [-1] * self.max_steps
-
-        observation = self.current_obs
+        observation = self._get_obs()
         return observation, {}
 
 
@@ -68,9 +66,17 @@ class GridWorldEnv(gym.Env):
         print("Action:", action)
         # We use `np.clip` to make sure we don't leave the grid bounds
         if action >= 0 and action < len(self.CLASS_LIST):
-            # Map the action (element of {0,1,2,3}) to the direction we walk in
-            if int(action) == self.target_chord[self.steps]:
-                reward = 1
+            # action 0 = 'no chords'
+            # action 1 = end current chord
+            if int(action) == 0 or int(action) == 1:
+                if int(action) == self.TARGET_CHORD[self.steps]:
+                    reward += 1
+            # Otherwise, assume guessing a chord:
+            elif self.idx_to_chord[int(action)] == self.TARGET_CHORD[self.steps]:
+                reward += 1
+            else:
+                reward = -1
+
         else: reward -= 1  
         self.steps             += 1
         observation             = self._get_obs()
@@ -78,8 +84,9 @@ class GridWorldEnv(gym.Env):
         else: terminated = False
         self.cumulative_reward += reward
         self.current_traj.append(observation)
-        truncated = ""
-        info = {"true path": self.target_chord}
+        truncated = False
+        info = {"true path": self.TARGET_CHORD}
+        print("New obs: ", observation)
         return observation, reward, terminated, truncated, info
     
     def save_trajectories(self):
